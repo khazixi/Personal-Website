8639c24cf032cb4d781992f17ad8c09abd77a4ed
Khazixi <khazixfangirl@gmail.com>
2023-11-06 18:14:24 -0500

    Added A toggle for light mode and dark mode

---
+++ main.js
@@ -0,0 +1,49 @@
+// INFO: Huge function, could maybe refactor?
+function toggle_dark() {
+  document
+    .querySelectorAll('.header')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document
+    .querySelectorAll('.article')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelectorAll('p')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelectorAll('h1')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelectorAll('h2')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelectorAll('h3')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelectorAll('h4')
+    .forEach(
+      (el) => el.classList.toggle('dark')
+    )
+
+  document.querySelector('body')
+    .classList.toggle('dark')
+}
+
+const btn = document.getElementById('toggler')
+
+btn.addEventListener(
+  'click',
+  () => toggle_dark()
+)
