d672d96758fff82419fc140ec35680a3f5c261e2
Khazixi <khazixfangirl@gmail.com>
2025-03-20 20:25:16 -0400

    Changed the css directory

--- _config.ts
+++ _config.ts
@@ -5,7 +5,8 @@ const site = lume({
   src: './src'
 });
 
-site.copy('_includes/styles')
+//site.copy('_includes/styles')
 site.use(lightningCss())
+site.copy('/static', '.')
 
 export default site;
